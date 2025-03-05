f7805f456f1976945775fecd1736c61776372939
SniT <trungtinnguyenhoang@192.168.1.30>
2025-03-05 12:36:12 +0700

    Update 050325

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Cấu hình trang
 st.set_page_config(
-    page_title="🏡 AI-Powered California Housing Price Prediction",
+    page_title="California Housing Price",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
